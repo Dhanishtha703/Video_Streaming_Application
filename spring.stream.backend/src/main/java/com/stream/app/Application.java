c19aed6bc350866e73f8096726ac6b31894846ff
Dhanishtha <Dhanishtha1813124@akgec.ac.in>
2024-08-05 14:58:29 +0530

    Perform db configuration

--- spring.stream.backend/src/main/java/com/stream/app/Application.java
+++ spring.stream.backend/src/main/java/com/stream/app/Application.java
@@ -1,4 +1,4 @@
-package com.stream.app.spring.stream.backend;
+package com.stream.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring.stream.backend/src/test/java/com/stream/app/ApplicationTests.java
+++ spring.stream.backend/src/test/java/com/stream/app/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stream.app.spring.stream.backend;
+package com.stream.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
